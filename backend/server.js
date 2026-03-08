e063dc52350b849ca27670cabb610c84d1a623a7
nipunkp <nipun3621@gmail.com>
2026-03-08 19:12:01 +0530

    deploy db to atlas

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/info',infoRoutes);
 app.use('/api/posts',postRoutes)
 mongoose
-  .connect('mongodb://127.0.0.1:27017/safetour')
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
@@ -177,7 +177,7 @@ app.get('/api/alerts', async (req, res) => {
 */
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
